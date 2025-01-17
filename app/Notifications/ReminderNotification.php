f90c73c089b0f982efd3ffc4639b2cec3c63bc9d
Thomas Neves <thomas_neves@ymail.com>
2025-01-17 02:32:07 -0300

    create reminder
    criado disparador de lembrete, mas o código apresenta erros de diferencia de horário do sistema, e não está encaminhado a mensagem.

---
+++ app/Notifications/ReminderNotification.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Notifications\Channels\WhatsAppChannel;
+use App\Notifications\Channels\WhatsAppMessage;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ReminderNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return [WhatsAppChannel::class];
+    }
+
+    public function toWhatsApp($notification)
+    {
+        return (new WhatsAppMessage)
+            ->contentSid("HX203c17d1d8562c38b77e6e7c4996c6df")
+            ->variables([]);
+    }
+}
--- app/Services/ConversationalService.php
+++ app/Services/ConversationalService.php
@@ -18,7 +18,7 @@ class ConversationalService{
         '!menu' => 'showMenu',
         '!agenda' => 'showSchedule',
         '!insights' => 'showInsights',
-        '!update' => 'updateUserTask'
+        '!update' => 'showUpdate'
     ];
 
     public function setUser(User $user): void
@@ -65,6 +65,46 @@ class ConversationalService{
         $this->user->notify(new ScheduleListNotification($tasks, $this->user->name));
     }
 
+    public function showInsights()
+    {
+        $now = now();
+        $messages = [
+            ["role" => "user", "content" => "Aja como um assistente pessoal, hoje é $now, entenda o historico de rotina do usuario e gere insights sobre sua rotina, onde ele pode melhorar e oque ele pode fazer para otimizar"],
+            ["role" => "function", "name" => 'getUserRoutine', "content" => $this->getUserRoutine()->tojson()],
+        ];
+
+        $this->talkToGpt($messages);
+    }
+
+ public function showUpdate()
+    {
+        $tasks = $this->user->tasks()->where('due_at', '>', now())->get();
+
+        $message = "Aqui esta a lista de tarefas que você pode atualizar: \n\n";
+
+        $message .= $tasks->reduce(function($carry, $item) {
+            return "{$carry}\n*ID: {$item->id}*: {$item->description}  às {$item->due_at->format('H:i')} no dia {$item->due_at->format('d/m')} e lembrete definido para {$item->reminder_at->format('d/m/Y H:i:s')}\n";
+        });
+
+
+        $message .= "\nDigite o ID da tarefa e o que voce deseja atualizar";
+
+
+        $this->user->memory = [
+            ['role' => 'assistant', 'content' => $message],
+        ];
+        $this->user->save();
+
+        $this->user->notify(new GenericNotification($message));
+    }
+
+    public function getUserRoutine($days = 30)
+    {
+        return $this->user->tasks()->where('due_at', '>', now()
+        ->subDays($days))
+        ->get();
+    }
+
     public function createUserTask($description, $due_at, $meta, $additional_info = "", $reminder_at = "")
     {
           $task = $this->user->tasks()->create([
@@ -76,6 +116,22 @@ class ConversationalService{
           ]);
 
           return $task;
+    }
+
+    public function updateUserTask(...$params)
+    {
+        /*if (!isset($params['taskid'])){
+            return "O campo 'taskid' é obrigatório";
+        }*/
+
+        $task = $this->user->tasks()->find($params['taskid']);
+
+        if ($task) {
+            return $task->update($params);
+        }
+
+        return "Task nao encontrada, usuario pode ter passado um id incorreto";
+
     }
 
     public function talkToGpt($messages, $clearMemory= false)
@@ -102,7 +158,7 @@ class ConversationalService{
                             ],
                             'meta' => [
                                 'type' => 'string',
-                                'description' => 'Metadados da tarefa solicitada pelo usuario que o chatgpt ache interessante para posteriormente gerar  insights sobre a rotina do usuario. Ex: reuniao de negocios: Discussao de projetos'
+                                'description' => 'Metadados da tarefa solicitada pelo usuario que o chatgpt ache interessante para posteriormente gerar  insights sobre a rotina do usuario este campo e obrigatorio. Ex: reuniao de negocios: Discussao de projetos'
                             ],
                             'reminder_at' => [
                                 'type' => 'string',
@@ -115,11 +171,56 @@ class ConversationalService{
                         ]
                     ],
                     'required' => 'description', 'due_at', 'meta'
+                ],
+                [
+                    'name' => 'updateUserTask',
+                    'description' => 'atualiza uma tarefa para um usuario',
+                    'parameters' => [
+                        'type' => 'object',
+                        'properties' => [
+                            'taskid' => [
+                                'type' => 'integer',
+                                'description' => 'ID da tarefa do usuario'
+                            ],
+                            'description' => [
+                                'type' => 'string',
+                                'description' => 'Nome da tarefa solicitada pelo usuario'
+                            ],
+                            'due_at' => [
+                                'type' => 'string',
+                                'description' => 'Data e hora da tarefa solicitada pelo usuario no formato Y-m-d H:i:s'
+                            ],
+                            'meta' => [
+                                'type' => 'string',
+                                'description' => 'Meta dados para a tarefa que o chat gpt ache interessante para posteriormente gerar insigths sobre produtividade do usuario Ex: Reunião de trabalho; discussão de projeto, Nunca pergunte sobre este campo, deduza sozinho'
+                            ],
+                            'reminder_at' => [
+                                'type' => 'string',
+                                'description' => 'Data e hora da do aviso que a tarefa esta chegando no formato Y-m-d H:i:s'
+                            ],
+                            'additional_info' => [
+                                'type' => 'string',
+                                'description' => 'Informações adicionais quem podem ou não serem solicitadas ao usuário'
+                            ]
+                        ]
+                    ],
+                    'required' => ['taskid', 'description', 'due_at', 'meta']
+                ],
+                [
+                    'name' => 'getUserRoutine',
+                    'description' => 'recupera as tarefas de um usuario dos ultimos dias passados por parametro, sendo 30 dias o padrao',
+                    'parameters' => [
+                        'type' => 'object',
+                        'properties' => [
+                            'days' => [
+                                'type' => 'integer',
+                                'description' => 'Quantidade de dias atras para recuperar as tarefas'
+                            ],
+                        ]
+                    ],
                 ]
             ]
         ]);
-
-        ds($messages);
 
         if (!isset($result->choices[0]->message->functionCall)){
             
